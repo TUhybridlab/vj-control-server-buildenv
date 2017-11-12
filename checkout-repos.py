e1d9946d8fc3bceda1066935a3aefd1f83c3172f
Juri Berlanda <juriberlanda@hotmail.com>
2017-11-12 16:18:11 +0100

    [Yocto] Update to rocko release.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- checkout-repos.py
+++ checkout-repos.py
@@ -4,8 +4,8 @@ import os
 
 from git import Git
 
-DEFAULT_BRANCH = "origin/master"
-YOCTO_RELEASE = "pyro"
+DEFAULT_BRANCH = "master"
+YOCTO_RELEASE = "rocko"
 
 yocto = {
 	'base_url': "git://git.yoctoproject.org/",
@@ -27,7 +27,7 @@ layers = [
 {
 	'base_url': "git@github.com:j-be/",
 	'name': "meta-vj",
-	'branch': YOCTO_RELEASE
+	'branch': DEFAULT_BRANCH
 }
 ]
 
@@ -46,6 +46,7 @@ def cloneOrCheckout(base_path, repo):
 	finally:
 		git.fetch()
 		git.checkout(repo['branch'])
+		git.rebase()
 		os.chdir(base_path)
 
 
